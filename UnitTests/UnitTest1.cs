ef9e33b4d041f226fa170fdc12f1a3432f0a34d9
Mikhail Tymchuk <mikhail.tymchuk@devexpress.com>
2016-02-05 17:29:34 +0300

    Unit tests added

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppVeyorTest;
+
+namespace UnitTests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            var expected = "Hello, world!";
+            var actual = new TestClass().TestMethod();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
